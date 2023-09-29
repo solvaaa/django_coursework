63587b12c069c1e4eb2eacc757dd26fac6cb8680
Alexandra <alexandrasolovjova@mail.ru>
2023-09-29 15:22:46 +0300

    add basic index page. project is running

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'mailing/base.html'
+    extra_context = {
+        'title': 'test'
+    }
