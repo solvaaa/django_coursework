ad8f488d48690c35ae3d9685901dc04164c14145
Alexandra <alexandrasolovjova@mail.ru>
2023-09-29 23:02:47 +0300

    add client views

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path
 from mailing.apps import MailingConfig
-from mailing.views import MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView
+from mailing.views import MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView, \
+    ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView
 
 appname = MailingConfig.name
 
 
 urlpatterns = [
-    path('', MailingListView.as_view(), name='list'),
-    path('<int:pk>', MailingDetailView.as_view(), name='detail'),
-    path('create/', MailingCreateView.as_view(), name='create'),
-    path('edit/<int:pk>', MailingUpdateView.as_view(), name='edit'),
-    path('delete/<int:pk>', MailingDeleteView.as_view(), name='delete'),
+    path('', MailingListView.as_view(), name='mailing_list'),
+    path('<int:pk>', MailingDetailView.as_view(), name='mailing_detail'),
+    path('create/', MailingCreateView.as_view(), name='mailing_create'),
+    path('edit/<int:pk>', MailingUpdateView.as_view(), name='mailing_edit'),
+    path('delete/<int:pk>', MailingDeleteView.as_view(), name='mailing_delete'),
+
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('client/<int:pk>', ClientDetailView.as_view(), name='client_detail'),
+    path('client/create/', ClientCreateView.as_view(), name='client_create'),
+    path('client/edit/<int:pk>', ClientUpdateView.as_view(), name='client_edit'),
+    path('client/delete/<int:pk>', ClientDeleteView.as_view(), name='client_delete'),
+
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from mailing.forms import MailingForm
-from mailing.models import Mailing
+from mailing.models import Mailing, Client
 
 
 # Create your views here.
@@ -22,7 +22,7 @@ class MailingCreateView(CreateView):
         'button_name': 'Добавить'
     }
     form_class = MailingForm
-    success_url = reverse_lazy('mailing:list')
+    success_url = reverse_lazy('mailing:mailing_list')
 
 
 class MailingUpdateView(UpdateView):
@@ -32,9 +32,42 @@ class MailingUpdateView(UpdateView):
         'button_name': 'Редактировать'
     }
     form_class = MailingForm
-    success_url = reverse_lazy('mailing:list')
+    success_url = reverse_lazy('mailing:mailing_list')
 
 
 class MailingDeleteView(DeleteView):
     model = Mailing
-    success_url = reverse_lazy('mailing:list')
+    success_url = reverse_lazy('mailing:mailing_list')
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    extra_context = {
+        'form_name': 'Добавление',
+        'button_name': 'Добавить'
+    }
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    extra_context = {
+        'form_name': 'Редактирование',
+        'button_name': 'Редактировать'
+    }
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('mailing:client_list')
