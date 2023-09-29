c50b79573d0714a4c203ad4a7041442391f62f91
Alexandra <alexandrasolovjova@mail.ru>
2023-09-29 14:15:28 +0300

    add urls for users

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from users.apps import UsersConfig
+
+
+app_name = UsersConfig.name
+
+
+urlpatterns = [
+
+]
