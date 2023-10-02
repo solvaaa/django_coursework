e585a9b9285897e1eee94ade09c8bd8ebfa93984
Alexandra <alexandrasolovjova@mail.ru>
2023-10-02 23:30:55 +0300

    add scheduler autostart setting

--- mailing/apps.py
+++ mailing/apps.py
@@ -1,11 +1,15 @@
 from django.apps import AppConfig
 import os
 
+from config.settings import SCHEDULER_AUTOSTART
+
+
 class MailingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailing'
 
     def ready(self):
         from mailing import scheduler
-        #scheduler.start()
+        if SCHEDULER_AUTOSTART:
+            scheduler.start()
         return super().ready()
