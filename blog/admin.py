2075c9e4e800db81450ef399916f1fa81c226331
Alexandra <alexandrasolovjova@mail.ru>
2023-10-02 23:46:13 +0300

    add blog model to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from blog.models import BlogPost
+from mailing.models import Mailing
+
+
 # Register your models here.
+@admin.register(BlogPost)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'views', 'pub_date')
+    sortable_by = ('pub_date', 'views', )
+    search_fields = ('title', 'content')
