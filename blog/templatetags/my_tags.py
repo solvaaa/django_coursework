00af39d24f807ed6c4ddbd3ead8b5faa03b63c7b
Alexandra <alexandrasolovjova@mail.ru>
2023-10-03 18:08:03 +0300

    fix clients list in mailing detail

--- blog/templatetags/my_tags.py
+++ blog/templatetags/my_tags.py
@@ -9,3 +9,12 @@ register = template.Library()
 def mediapath(text):
     return f'{MEDIA_URL}{text}'
 
+
+@register.simple_tag
+def get_list_from_queryset(queryset):
+    query_list = list(queryset)
+    str_list = []
+    for item in query_list:
+        str_list.append(str(item))
+    query_str = '\n'.join(str_list)
+    return query_str
